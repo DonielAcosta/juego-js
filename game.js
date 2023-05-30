9c0a3cc9fe962c3984a471de477b412b25ea8723
doniel <donielacosta1995@gmail.com>
2023-05-30 11:56:36 -0400

    creacion de inicio de juego

--- game.js
+++ game.js
@@ -1,17 +1,34 @@
-// console.log(maps);
-
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load',startGame);
-function startGame(){
-    // game.fillRect(0,0,100,100);
-    // game.clearRect(50,50,50,50);
-    // game.clearRect();
-    // game.clearRect(50,50,50,50);
+let canvasSize;
+let elementsSize;
 
-    game.font = '25px Verdana';
-    game.fillStyle = 'purple';
-    game.textAlign = 'ring';
-    game.fillText('ProteoERP',25,25);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
+
+function startGame() {
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
 }
