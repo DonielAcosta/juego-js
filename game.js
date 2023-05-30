675c85703989df784ad38f7f4e2183140d738108
doniel <donielacosta1995@gmail.com>
2023-05-30 09:29:44 -0400

    juego

---
+++ game.js
@@ -0,0 +1,17 @@
+// console.log(maps);
+
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load',startGame);
+function startGame(){
+    // game.fillRect(0,0,100,100);
+    // game.clearRect(50,50,50,50);
+    // game.clearRect();
+    // game.clearRect(50,50,50,50);
+
+    game.font = '25px Verdana';
+    game.fillStyle = 'purple';
+    game.textAlign = 'ring';
+    game.fillText('ProteoERP',25,25);
+}
